2031b06372813f11ceeb9bea01e2104e6e843bbc
swayamsivam37 <swayam.sivam37@gmail.com>
2025-07-23 14:26:45 +0530

    Initialized the frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, updateCount] = useState(0);
+  return (
+      <div>
+        Hi! there 
+    </div>
+  )
+}
+
+export default App
